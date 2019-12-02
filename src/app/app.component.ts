ad32e55b7c752a3a0b671cac42a90a9d19fe9e88
orzio <andrzej.orzinski@gmail.com>
2019-12-02 14:36:07 +0100

    add group by function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Output , Input} from '@angular/core';
 import * as data from './movies.json';
-
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-root',
@@ -8,24 +8,37 @@ import * as data from './movies.json';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  constructor(){}
+
+  ngOnInit(){
+    for(let i=0; i<this.counter; i++)
+        this.moviesToDisplay.push(this.movies[i]);
+
+  this.fillMoviesArrays();
+  this.fillMoviesByGenersArray();
+  this.groupArray(this.genersGroup, 'gener');
+  console.log(this.grouppedMoviesByGenersArray);
+}
   title = 'zadanie-trzecie';
   diffrenceBetweenMoviesAndMoviesToDisplayLength:number;
   movies:any= (data as any).default;
-  moviesToDisplay:{title:string, year:number, cast:[],geners:[]}[] = [];
+  moviesToDisplay:{title:string, year:number, cast:[],genres:[]}[] = [];
+  moviesBygeners:{title:string, year:number, cast:[],genres:[]}[] = [];
+  moviesByCast:{title:string, year:number, cast:[],genres:[]}[] = [];
+  genersGroup:any = [];
   counter:number =10;
+  grouppedMoviesByGenersArray:any =[];
 
-  ngOnInit(){
-    for(let i=0; i<this.counter; i++){
-      this.moviesToDisplay.push(this.movies[i]);
-  }
-}
-  constructor(){
-  }
 
+
+
+
+
+  /**Functions */
 
   onAddPositions(){
     console.log('odebrane');
-
     let prevCounter = this.counter;
     this.diffrenceBetweenMoviesAndMoviesToDisplayLength = this.movies.length - this.counter;
     if( this.diffrenceBetweenMoviesAndMoviesToDisplayLength > 10){
@@ -37,5 +50,42 @@ export class AppComponent {
       this.moviesToDisplay.push(this.movies[prevCounter]);
     }
   }
+
+  fillMoviesArrays(){
+    let counter =0;
+    for(let movie of this.movies){
+      if(counter<=100){
+        this.moviesBygeners.push(movie);
+        this.moviesByCast.push(movie);
+        counter++;
+      }else break;
+    }
+  }
+
+
+  fillMoviesByGenersArray(){
+    for(let movie of this.moviesBygeners){
+      if(movie.genres.length ==0){
+        console.log("w ifie");
+        this.genersGroup.push({title:movie.title,
+          gener:"Unknown"});
+          continue;
+      }else{
+      for(let count =0; count < movie.genres.length; count++){
+        this.genersGroup.push({title:movie.title,
+                                gener:movie.genres[count]});
+      }
+      }
+    }
+  }
+
+  groupArray(list, selector){
+    this.grouppedMoviesByGenersArray = _.groupBy(list, selector);
+  }
+
+
+
+
+
 
 }
